8fab96d06d187edf3c673f66bbd68530c363c774
Mia Bennett <mia@chillfox.com>
2022-10-11 13:46:51 +0930

    add driver

---
+++ drivers/nothing/nothing.cr
@@ -0,0 +1,29 @@
+require "placeos-driver"
+
+class Nothing < PlaceOS::Driver
+  generic_name :Nothing
+  descriptive_name "Driver that does nothing"
+  default_settings({
+    do_something: false,
+  })
+
+  def on_load
+    logger.info { "Nothing #on_load" }
+  end
+
+  def on_update
+    logger.info { "Nothing #on_update" }
+  end
+
+  def on_unload
+    logger.info { "Nothing #on_unload" }
+  end
+
+  def connected
+    logger.info { "Nothing #connected" }
+  end
+
+  def disconnected
+    logger.info { "Nothing #disconnected" }
+  end
+end
--- drivers/place/edge_demo_spec.cr
+++
@@ -1,6 +0,0 @@
-require "placeos-driver/spec"
-
-DriverSpecs.mock_driver "Place::EdgeDemo" do
-  exec(:power, true).get
-  status[:power].should eq(true)
-end
