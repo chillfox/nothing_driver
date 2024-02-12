01cd80d94263d8c7dad2b2ad2d149ead945694f3
Mia Bennett <mia@chillfox.com>
2024-02-12 04:48:36 +0000

    feat: test method

--- drivers/nothing/nothing.cr
+++ drivers/nothing/nothing.cr
@@ -7,12 +7,17 @@ class Nothing < PlaceOS::Driver
     do_something: false,
   })
 
+  @do_something : Bool = false
+
   def on_load
     logger.info { "Nothing #on_load" }
   end
 
   def on_update
     logger.info { "Nothing #on_update" }
+
+    @do_something = setting?(Bool, :do_something) || false
+    schedule.every(5.minutes) { pretend_to_do_something } if do_something
   end
 
   def on_unload
@@ -25,5 +30,14 @@ class Nothing < PlaceOS::Driver
 
   def disconnected
     logger.info { "Nothing #disconnected" }
+  end
+
+  def pretend_to_do_something
+    logger.notice { "Nothing pretending to do something!" }
+  end
+
+  # Test methods
+  def get_system_zones
+    system.zones
   end
 end
